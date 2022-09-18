63fbc383ea88b4c507c9697c9a8eeba9a083ed7d
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-17 20:31:15 -0500

    add loops for error messages until correct characters and numbers chosen and add remaining comments and clean up spacing

--- script.js
+++ script.js
@@ -7,46 +7,52 @@ var upperCaseLtrs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 var specialChars = ["!", "@","#", "$", "%", "&", "_", "*", "+", "-", "^", "~",] ;
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-// ask the user how many characters should be in the password
+// ask the user how many characters should be in the password and loop if invalid
 function pwOptions(){
-  var pwLength = parseInt( prompt("How many characters?") );
-  if (pwLength > 128 || pwLength < 8 || isNaN(pwLength)) {
-    window.alert("Please enter a number that must be at least 8 characters and no more than 129 characters");
-      return;}
+  var pwLength;
+  do {
+    pwLength = parseInt( prompt("How many characters between 8 and 128?") );
+    var invalid = pwLength > 128 || pwLength < 8 || isNaN(pwLength);
+    if (invalid) {
+      window.alert("Please enter a number that must be at least 8 characters and no more than 129 characters");
+    }
+  } while (invalid)
   console.log(pwLength);
 
+  do {
   // ask user if they want lower case letters in the password
- 
-  var useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
-  console.log(useLowerCaseLtrs)
+    var useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
+    console.log(useLowerCaseLtrs)
+
+  // ask user if they want upper case letters in the password  
+    var useUpperCaseLtrs = confirm("Do you want upper case letters in the password?");
+    console.log(useUpperCaseLtrs)
+
+  // ask user if they want special characters in the password
+    var useSpecialCharacters = confirm("Do you want special characters in the password?");
+    console.log(useSpecialCharacters)
+
+  // ask user if they want numbers in the password
+    var useNumbers = confirm("Do you want numbers in the password?");
+    console.log(useNumbers)
+
+  //display error and loop if at least one different character is not selected  
+    var invalid =  !useLowerCaseLtrs && !useUpperCaseLtrs && !useSpecialCharacters && !useNumbers 
+    if (invalid) {  
+    window.alert("At least one character type is required for the password");
+    }
+  } while(invalid)
 
 
-// ask user if they want upper case letters in the password  
-  var useUpperCaseLtrs = confirm("Do you want upper case letters in the password?");
-  console.log(useUpperCaseLtrs)
-
-// ask user if they want special characters in the password
-  var useSpecialCharacters = confirm("Do you want special characters in the password?");
-  console.log(useSpecialCharacters)
-
-// ask user if they want numbers in the password
-  var useNumbers = confirm("Do you want numbers in the password?");
-  console.log(useNumbers)
-    if (!useLowerCaseLtrs && !useUpperCaseLtrs && !useSpecialCharacters && !useNumbers) {
-      window.alert("At least one character type is required for the password");
-    return;//Do I need to add var name here?
-}
-
-//I have questions about this for Gene Paul
+//Define object for length, upper , lower,specialChars, and numberas
   var passwordOptions = {
     pwLength: pwLength,
     useLowerCaseLtrs: useLowerCaseLtrs,
     useUpperCaseLtrs: useUpperCaseLtrs,
     useSpecialCharacters: useSpecialCharacters,
     useNumbers: useNumbers
-};
+  };
 return passwordOptions;
-
 }
 //Creating a random element from an array
 function makeRandom(arr) {
@@ -58,38 +64,37 @@ function makeRandom(arr) {
 
 // function to generate password
 function generatePassword() {
-  // console.log("start here");
   var options = pwOptions();
   var finalPassword = [];
   var possibleOptions = [];
   var combinedOptions = [];
 
 //adding a random element to the array of possible options and combined options
-if (options.useLowerCaseLtrs) {
-  possibleOptions = possibleOptions.concat(lowerCaseLtrs);
-  combinedOptions.push(makeRandom(lowerCaseLtrs))
-}
-if (options.useUpperCaseLtrs) {
-  possibleOptions = possibleOptions.concat(upperCaseLtrs);
-  combinedOptions.push(makeRandom(upperCaseLtrs))
-}
-if (options.useSpecialCharacters) {
-  possibleOptions = possibleOptions.concat(specialChars);
-  combinedOptions.push(makeRandom(specialChars))
-}
-if (options.useNumbers) {
-  possibleOptions = possibleOptions.concat(numbers);
-  combinedOptions.push(makeRandom(numbers))
-}
-for (i = 0; i <options.pwLength; i++) {
-  var possibleCharacter = makeRandom(possibleOptions);
-  finalPassword.push(possibleCharacter);
-}
+  if (options.useLowerCaseLtrs) {
+    possibleOptions = possibleOptions.concat(lowerCaseLtrs);
+    combinedOptions.push(makeRandom(lowerCaseLtrs))
+  }
+  if (options.useUpperCaseLtrs) {
+    possibleOptions = possibleOptions.concat(upperCaseLtrs);
+    combinedOptions.push(makeRandom(upperCaseLtrs))
+  }
+  if (options.useSpecialCharacters) {
+    possibleOptions = possibleOptions.concat(specialChars);
+    combinedOptions.push(makeRandom(specialChars))
+  }
+  if (options.useNumbers) {
+    possibleOptions = possibleOptions.concat(numbers);
+    combinedOptions.push(makeRandom(numbers))
+  }
+  for (i = 0; i <options.pwLength; i++) {
+    var possibleCharacter = makeRandom(possibleOptions);
+    finalPassword.push(possibleCharacter);
+  }
 
-for (i = 0; i <combinedOptions.length; i++) {
-  finalPassword[i] = combinedOptions[i];
-}
-return finalPassword.join("");
+  for (i = 0; i <combinedOptions.length; i++) {
+    finalPassword[i] = combinedOptions[i];
+  }
+  return finalPassword.join("");
 }
 
 var generateBtn = document.querySelector("#generate");
@@ -98,9 +103,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
